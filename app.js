6b2729a37caf62a89c9b4537486ffe7405d1ae57
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-02-28 23:40:53 +0330

    Add post request

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var userName = req.body.userName;
+  var userEmail = req.body.userEmail;
+  console.log(userName, userEmail);
 });
 
 app.listen(3000, () => {
