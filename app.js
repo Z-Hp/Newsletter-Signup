029883f0e97ee73b89cbe92e5b6be8a1cd6e7da4
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-03-02 19:34:42 +0330

    Add success and failure pages

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ app.post("/", (req, res) => {
   var userName = req.body.userName;
   var userEmail = req.body.userEmail;
   console.log(userName, userEmail);
+  if (userName === "" || userEmail === "") {
+    res.sendFile(__dirname + "/failure.html");
+  } else {
+    res.sendFile(__dirname + "/success.html");
+  }
 });
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+// app.post("/success", (req, res) => {
+//   res.redirect("/");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
